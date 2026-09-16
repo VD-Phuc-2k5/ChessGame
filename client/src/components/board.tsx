998894044db5d3ba0d6a44c5f7a789bb7a028874
VD-Phuc-2k5 <duyvo1954@gmail.com>
2026-09-16 13:01:44 +0700

    refactor: optimize BoardComponent and CellComponent for better rendering and structure

--- client/src/components/board.tsx
+++ client/src/components/board.tsx
@@ -1,6 +1,6 @@
 import { JSX } from 'react';
 
-import { ICell } from '@chess/core';
+import { FILE_ASCII_OFFSET } from '@chess/core';
 import { Board } from '@chess/domain';
 
 import CellComponent from './cell';
@@ -8,36 +8,45 @@ import CellComponent from './cell';
 function BoardComponent(): JSX.Element {
   const boardSize = 500;
   const board = Board.getBoardInstance(boardSize);
+  const cells: JSX.Element[] = [];
+
+  for (const cell of board) {
+    const coordinate: string = cell.getCoordinate();
+    const color: string = cell.getColor();
+    cells.push(
+      <div key={coordinate} className="grid h-full w-full place-items-center">
+        <CellComponent color={color} coordinate={coordinate} />
+      </div>
+    );
+  }
+
   return (
     <section className="flex min-h-screen w-full items-center justify-center">
-      <div>
-        <div className="flex h-full w-full">
+      <div className="flex flex-col items-end">
+        <div className="flex h-full w-full items-end">
           {/* File Number */}
-          <div className="flex w-8 flex-col">
+          <div className="grid w-8 grid-rows-8" style={{ height: boardSize }}>
             {Array.from({ length: 8 }, (_: unknown, index: number): JSX.Element => (
-              <div key={index} className="flex h-full w-full items-center justify-center">
+              <div key={index} className="grid h-full place-items-center">
                 <span className="text-lg font-bold">{8 - index}</span>
               </div>
             ))}
           </div>
           {/* Board */}
-          <div style={{ width: boardSize, height: boardSize }} className="flex flex-col border-4">
-            {board.cells.map((row: ICell[], rowIndex: number): JSX.Element => (
-              <div key={rowIndex} className="flex h-full w-full">
-                {row.map((cell: ICell, cellIndex: number): JSX.Element => (
-                  <div key={cellIndex} className="h-full w-full">
-                    <CellComponent color={cell.getColor()} />
-                  </div>
-                ))}
-              </div>
-            ))}
+          <div
+            style={{ width: boardSize, height: boardSize }}
+            className="grid grid-cols-8 border-4"
+          >
+            {cells}
           </div>
         </div>
         {/* Rank */}
-        <div className="flex h-full w-full">
+        <div className="grid grid-cols-8" style={{ width: boardSize, height: 32 }}>
           {Array.from({ length: 8 }, (_: unknown, index: number): JSX.Element => (
-            <div key={index} className="flex h-full w-full items-center justify-center">
-              <span className="text-lg font-bold">{String.fromCharCode(65 + index)}</span>
+            <div key={index} className="grid h-8 w-full place-items-center">
+              <span className="text-lg font-bold">
+                {String.fromCharCode(FILE_ASCII_OFFSET + index + 1)}
+              </span>
             </div>
           ))}
         </div>
--- client/src/components/cell.tsx
+++ client/src/components/cell.tsx
@@ -2,10 +2,15 @@ import { JSX } from 'react';
 
 interface CellComponentProps {
   color: string;
+  coordinate: string;
 }
 
 function CellComponent(props: CellComponentProps): JSX.Element {
-  return <div style={{ backgroundColor: props.color }} className="h-full w-full"></div>;
+  return (
+    <div style={{ backgroundColor: props.color }} className="grid h-full w-full place-items-center">
+      {props.coordinate}
+    </div>
+  );
 }
 
 export default CellComponent;
