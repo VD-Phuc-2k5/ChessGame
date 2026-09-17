ee1f044f09f592e71ce01adca038b814338cd296
VD-Phuc-2k5 <duyvo1954@gmail.com>
2026-09-18 01:35:28 +0700

    refactor: remove unused 'use client' directive and add priority to image loading in CellComponent

--- client/src/components/board/cell.tsx
+++ client/src/components/board/cell.tsx
@@ -15,7 +15,7 @@ function CellComponent(props: CellComponentProps): JSX.Element {
   const imgSrc: string | null = symbol ? `/assets/${symbol}.png` : null;
   return (
     <div style={{ backgroundColor: props.color }} className="grid h-full w-full place-items-center">
-      {imgSrc && <Image src={imgSrc} alt={imgSrc} width={40} height={40} />}
+      {imgSrc && <Image src={imgSrc} alt={imgSrc} width={40} height={40} priority />}
     </div>
   );
 }
