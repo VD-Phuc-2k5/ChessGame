37b8bae5c1bc52d81f2c7d8b966a34a8a3da0972
VD-Phuc-2k5 <duyvo1954@gmail.com>
2026-09-19 15:26:53 +0700

    refactor: implement BishopMovement class and update Bishop piece to utilize movement table

---
+++ packages/domain/src/lib/movement/bishopMovement.ts
@@ -0,0 +1,40 @@
+import { MoveTableType, IPosition, START_RANK, START_FILE, END_RANK, END_FILE } from '@chess/core';
+import { SlidingMovement } from './slidingMovement.js';
+import { Position } from '../position/position.js';
+
+class BishopMovement extends SlidingMovement {
+  private static instance: BishopMovement | null;
+  public readonly moveTable: MoveTableType;
+
+  private constructor() {
+    super();
+    this.moveTable = this.buildMoveTable();
+  }
+
+  public static getMovement(): BishopMovement {
+    if (!BishopMovement.instance) {
+      BishopMovement.instance = new BishopMovement();
+    }
+    return BishopMovement.instance;
+  }
+
+  public buildMoveTable(): MoveTableType {
+    const moveTable: MoveTableType = new Map();
+
+    for (let rankNumber: number = START_RANK; rankNumber <= END_RANK; rankNumber++) {
+      for (let fileNumber: number = START_FILE; fileNumber <= END_FILE; fileNumber++) {
+        const square: IPosition = Position.of(rankNumber, fileNumber);
+        const slidingMoves: IPosition[] = this.getSlidingMoves(rankNumber, fileNumber, [
+          [1, 1],
+          [1, -1],
+          [-1, 1],
+          [-1, -1],
+        ]);
+        moveTable.set(square.toString(), slidingMoves);
+      }
+    }
+    return moveTable;
+  }
+}
+
+export { BishopMovement };
