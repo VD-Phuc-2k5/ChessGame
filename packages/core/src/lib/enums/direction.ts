14c5ca26224df433c329de9cd44bf257038390ac
VD-Phuc-2k5 <duyvo1954@gmail.com>
2026-09-19 16:14:54 +0700

    refactor: add Direction enum and OffsetType type, update exports in index files

---
+++ packages/core/src/lib/enums/direction.ts
@@ -0,0 +1,20 @@
+enum Direction {
+  Left = 'left',
+  Right = 'right',
+  Top = 'top',
+  Bottom = 'bottom',
+  TopLeft = 'topLeft',
+  TopRight = 'topRight',
+  BottomLeft = 'bottomLeft',
+  BottomRight = 'bottomRight',
+  Left2Top = 'left2Top',
+  Left2Bottom = 'left2Bottom',
+  Right2Top = 'right2Top',
+  Right2Bottom = 'right2Bottom',
+  LeftTop2 = 'leftTop2',
+  RightTop2 = 'rightTop2',
+  LeftBottom2 = 'leftBottom2',
+  RightBottom2 = 'rightBottom2',
+}
+
+export { Direction };
